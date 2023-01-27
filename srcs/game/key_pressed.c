75603a511c244be7fffa093708578815c586b882
Younganswer <dudtj1999@gmail.com>
2023-01-27 17:48:47 +0900

    .

--- srcs/game/key_pressed.c
+++ srcs/game/key_pressed.c
@@ -1,96 +1,51 @@
 #include "../../incs/game.h"
-#include <stdio.h>
+#include <math.h>
 
 const static double	g_move_speed = 0.03;
 
 int				key_pressed(int keycode, t_game *game);
-static t_bool	move(t_keycode keycode, t_game *game);
-
-static int		print_game(int key, t_game *game);
-static t_bool	print_space(t_game *game);
-static t_bool	print_map(t_game *game);
-static t_bool	print_player(t_game *game);
+static t_bool	move_to(t_keycode keycode, t_game *game);
+static t_bool	move(t_game *game, double dx, double dy);
 
 int	key_pressed(int keycode, t_game *game)
 {
 	if (keycode == KEY_ESC)
 		exit_game(game);
-	if (keycode == KEY_RETURN)
-		print_game(keycode, game);
 	if (keycode == KEY_W || \
 		keycode == KEY_A || \
 		keycode == KEY_S || \
 		keycode == KEY_D)
-		move(keycode, game);
+		move_to(keycode, game);
 	return (0);
 }
 
-static t_bool	move(t_keycode keycode, t_game *game)
+static t_bool	move_to(t_keycode keycode, t_game *game)
 {
+	const double	dx = game->player->dir.x * g_move_speed;
+	const double	dy = game->player->dir.y * g_move_speed;
+
 	if (keycode == KEY_W)
-	{
-		if (game->worldmap->map[(int)(game->player->pos.x + game->player->dir.x * g_move_speed)][(int)(game->player->pos.y)] == '0')
-			game->player->pos.x += game->player->dir.x * g_move_speed;
-		if (game->worldmap->map[(int)(game->player->pos.x)][(int)(game->player->pos.y + game->player->dir.y * g_move_speed)] == '0')
-			game->player->pos.y += game->player->dir.y * g_move_speed;
-	}
+		move(game, dx, dy);
 	else if (keycode == KEY_S)
-	{
-		if (game->worldmap->map[(int)(game->player->pos.x - game->player->dir.x * g_move_speed)][(int)(game->player->pos.y)] == '0')
-			game->player->pos.x -= game->player->dir.x * g_move_speed;
-		if (game->worldmap->map[(int)(game->player->pos.x)][(int)(game->player->pos.y - game->player->dir.y * g_move_speed)] == '0')
-			game->player->pos.y -= game->player->dir.y * g_move_speed;
-	}
+		move(game, -dx, -dy);
 	else if (keycode == KEY_A)
-	{
-		if (game->worldmap->map[(int)(game->player->pos.x - game->player->dir.y * g_move_speed)][(int)(game->player->pos.y)] == '0')
-			game->player->pos.x -= game->player->dir.y * g_move_speed;
-		if (game->worldmap->map[(int)(game->player->pos.x)][(int)(game->player->pos.y + game->player->dir.x * g_move_speed)] == '0')
-			game->player->pos.y += game->player->dir.x * g_move_speed;
-	}
+		move(game, -dy, dx);
 	else if (keycode == KEY_D)
-	{
-		if (game->worldmap->map[(int)(game->player->pos.x + game->player->dir.y * g_move_speed)][(int)(game->player->pos.y)] == '0')
-			game->player->pos.x += game->player->dir.y * g_move_speed;
-		if (game->worldmap->map[(int)(game->player->pos.x)][(int)(game->player->pos.y - game->player->dir.x * g_move_speed)] == '0')
-			game->player->pos.y -= game->player->dir.x * g_move_speed;
-	}
+		move(game, dy, -dx);
 	return (TRUE);
 }
 
-static int	print_game(int key, t_game *game)
+static t_bool	move(t_game *game, double dx, double dy)
 {
-	(void) key;
-	print_space(game);
-	print_map(game);
-	print_player(game);
-	return (TRUE);
-}
+	t_player *const	player = game->player;
+	const double	correction_x = player->pos.x + (0.1) * (dx / fabs(dx));
+	const double	correction_y = player->pos.y + (0.1) * (dy / fabs(dy));
+	const double	new_x = player->pos.x + dx;
+	const double	new_y = player->pos.y + dy;
 
-static t_bool	print_space(t_game *game)
-{
-	printf("NORTH: %s\n", game->space->walls[0].file);
-	printf("SOUTH: %s\n", game->space->walls[1].file);
-	printf("WEST: %s\n", game->space->walls[2].file);
-	printf("EAST: %s\n", game->space->walls[3].file);
-	printf("FLOOR: (%lu, %lu, %lu)\n", game->space->floor_color >> 16 & 0xFF, (game->space->floor_color >> 8) & 0xFF, game->space->floor_color & 0xFF);
-	printf("CEILING: (%lu, %lu, %lu)\n", game->space->ceiling_color >> 16 & 0xFF, (game->space->ceiling_color >> 8) & 0xFF, game->space->ceiling_color & 0xFF);
-	return (TRUE);
-}
-
-static t_bool	print_map(t_game *game)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < game->worldmap->height)
-		printf("%s\n", game->worldmap->map[i++]);
-	return (TRUE);
-}
-
-static t_bool	print_player(t_game *game)
-{
-	printf("PLAYER POS: (%f, %f)\n", game->player->pos.x, game->player->pos.y);
-	printf("PLAYER DIR: (%f, %f)\n", game->player->dir.x, game->player->dir.y);
+	if (game->worldmap->map[(int)correction_x][(int)player->pos.y] == '0')
+		player->pos.x = new_x;
+	if (game->worldmap->map[(int)player->pos.x][(int)correction_y] == '0')
+		player->pos.y = new_y;
 	return (TRUE);
 }
