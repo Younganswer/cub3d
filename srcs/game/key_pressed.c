3b1172ebc3eb3260eff4b4d65fba46d4fb643212
Younganswer <dudtj1999@gmail.com>
2023-01-28 17:13:17 +0900

    .

--- srcs/game/key_pressed.c
+++ srcs/game/key_pressed.c
@@ -1,29 +1,23 @@
 #include "../../incs/game.h"
 #include <math.h>
+#include <stdlib.h>
 
 const static double	g_move_speed = 0.03;
+const static double	g_correction_dist = 0.2;
+const static double	g_offset = 0.01;
 
 int				key_pressed(int keycode, t_game *game);
-static t_bool	move_to(t_keycode keycode, t_game *game);
 static t_bool	move(t_game *game, double dx, double dy);
+static t_bool	set_next_x(t_game *game, t_coord *next, double dx);
+static t_bool	set_next_y(t_game *game, t_coord *next, double dy);
 
 int	key_pressed(int keycode, t_game *game)
-{
-	if (keycode == KEY_ESC)
-		exit_game(game);
-	if (keycode == KEY_W || \
-		keycode == KEY_A || \
-		keycode == KEY_S || \
-		keycode == KEY_D)
-		move_to(keycode, game);
-	return (0);
-}
-
-static t_bool	move_to(t_keycode keycode, t_game *game)
 {
 	const double	dx = game->player->dir.x * g_move_speed;
 	const double	dy = game->player->dir.y * g_move_speed;
 
+	if (keycode == KEY_ESC)
+		exit_game(game);
 	if (keycode == KEY_W)
 		move(game, dx, dy);
 	else if (keycode == KEY_S)
@@ -32,20 +26,65 @@ static t_bool	move_to(t_keycode keycode, t_game *game)
 		move(game, -dy, dx);
 	else if (keycode == KEY_D)
 		move(game, dy, -dx);
-	return (TRUE);
+	return (0);
 }
 
 static t_bool	move(t_game *game, double dx, double dy)
 {
 	t_player *const	player = game->player;
-	const double	correction_x = player->pos.x + (0.1) * (dx / fabs(dx));
-	const double	correction_y = player->pos.y + (0.1) * (dy / fabs(dy));
-	const double	new_x = player->pos.x + dx;
-	const double	new_y = player->pos.y + dy;
+	t_coord *const	next = ft_calloc(sizeof(t_coord), 1, "");
 
-	if (game->worldmap->map[(int)correction_x][(int)player->pos.y] == '0')
-		player->pos.x = new_x;
-	if (game->worldmap->map[(int)player->pos.x][(int)correction_y] == '0')
-		player->pos.y = new_y;
+	if (set_next_x(game, next, dx) == FALSE || \
+		set_next_y(game, next, dy) == FALSE)
+		return (FALSE);
+	player->pos.x = next->x;
+	player->pos.y = next->y;
+	free(next);
+	return (TRUE);
+}
+
+static t_bool	set_next_x(t_game *game, t_coord *next, double dx)
+{
+	const double	cur_x = game->player->pos.x;
+	const double	cur_y = game->player->pos.y;
+
+	next->x = cur_x + dx;
+	if ((dx < 0) && (next->x - (int)cur_x < g_correction_dist) && \
+		((game->worldmap->map[(int)(cur_x - 1)][(int)cur_y] == '1') || \
+		((cur_y - (int)cur_y < g_correction_dist - g_offset) && \
+		(game->worldmap->map[(int)(cur_x - 1)][(int)(cur_y - 1)] == '1')) || \
+		((1 - g_correction_dist + g_offset < cur_y - (int)cur_y) && \
+		(game->worldmap->map[(int)(cur_x - 1)][(int)(cur_y + 1)] == '1'))))
+		next->x = (int)cur_x + g_correction_dist;
+	else if ((0 <= dx) && (1 - g_correction_dist < next->x - (int)cur_x) && \
+		((game->worldmap->map[(int)(cur_x + 1)][(int)cur_y] == '1') || \
+		((cur_y - (int)cur_y < g_correction_dist - g_offset) && \
+		(game->worldmap->map[(int)(cur_x + 1)][(int)(cur_y - 1)] == '1')) || \
+		((1 - g_correction_dist + g_offset < cur_y - (int)cur_y) && \
+		(game->worldmap->map[(int)(cur_x + 1)][(int)(cur_y + 1)] == '1'))))
+		next->x = (int)(cur_x + 1) - g_correction_dist;
+	return (TRUE);
+}
+
+static t_bool	set_next_y(t_game *game, t_coord *next, double dy)
+{
+	const double	cur_x = game->player->pos.x;
+	const double	cur_y = game->player->pos.y;
+
+	next->y = cur_y + dy;
+	if ((dy < 0) && (next->y - (int)cur_y < g_correction_dist) && \
+		((game->worldmap->map[(int)cur_x][(int)(cur_y - 1)] == '1') || \
+		((cur_x - (int)cur_x < g_correction_dist - g_offset) && \
+		(game->worldmap->map[(int)(cur_x - 1)][(int)(cur_y - 1)] == '1')) || \
+		((1 - g_correction_dist + g_offset < cur_x - (int)cur_x) && \
+		(game->worldmap->map[(int)(cur_x + 1)][(int)(cur_y - 1)] == '1'))))
+		next->y = (int)cur_y + g_correction_dist;
+	else if ((0 <= dy) && (1 - g_correction_dist < next->y - (int)cur_y) && \
+		((game->worldmap->map[(int)cur_x][(int)(cur_y + 1)] == '1') || \
+		((cur_x - (int)cur_x < g_correction_dist - g_offset) && \
+		(game->worldmap->map[(int)(cur_x - 1)][(int)(cur_y + 1)] == '1')) || \
+		((1 - g_correction_dist + g_offset < cur_x - (int)cur_x) && \
+		(game->worldmap->map[(int)(cur_x + 1)][(int)(cur_y + 1)] == '1'))))
+		next->y = (int)(cur_y + 1) - g_correction_dist;
 	return (TRUE);
 }
