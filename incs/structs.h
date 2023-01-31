8a6cb1abca7a85b864a0aeb5c8b274cd985f4a46
Younganswer <younganswer@kookmin.ac.kr>
2023-01-31 15:35:54 +0900

    .

--- incs/structs.h
+++ incs/structs.h
@@ -3,12 +3,10 @@
 
 # include "../libs/libft/incs/libft.h"
 
-// # define SCREEN_WIDTH 1280
-// # define SCREEN_HEIGHT 720
+# define SCREEN_WIDTH 1280
+# define SCREEN_HEIGHT 720
 //# define SCREEN_WIDTH 1920
 //# define SCREEN_HEIGHT 1080
-# define SCREEN_WIDTH (16 * 150)
-# define SCREEN_HEIGHT (9 * 150)
 
 typedef enum e_err
 {
